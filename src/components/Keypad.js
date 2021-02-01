9dac37f2f653480e33dd58cb74ae4952284303b5
Sarah Thiery <sarahvthiery@gmail.com>
2021-02-01 17:08:26 -0500

    Passed tests

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad() {
+
+    function change() {
+        console.log('Entering password...')
+    }
+
+    return (
+        <input type="password" onChange={change}/>
+    )
+}
+
+export default Keypad;
